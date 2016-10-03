5ddc78e9c99a58010edc3488c1b8a2bbca6d065b
Codes and Tags <codesandtags@gmail.com>
2016-10-02 23:52:36 -0500

    feat: Adding support for Handlebars Engine Template
    Was added and set up the engine to use HandleBars.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('express-hbs');
+var handlebars = require('handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,9 +13,21 @@ var meetups = require('./routes/meetups');
 
 var app = express();
 
+//uncomment the next line if you want to use jade engine
+//app.set('view engine', 'jade');
+
+//Handlebars engine template
+/*
+app.engine('hbs', hbs.express4({
+    //partialsDir   : __dirname +'/dist/views/partials',
+    defaultLayout : __dirname +'/dist/views/index',
+    extname       : '.hbs',
+    layoutsDir    : __dirname +'/dist/views/',
+}));
+*/
+app.set('view engine', 'hbs');
 // view engine setup
-app.set('views', path.join(__dirname, './dist/views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'app/views'));
 
 // uncomment after placing your favicon in /dist
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
