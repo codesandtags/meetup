6130063fdc011149ae11cea5bbae0d37d998cf30
Codes and Tags <codesandtags@gmail.com>
2016-10-06 15:18:05 -0500

    feat: Adding meetup cards
    - Was cards which are render using a JSON object in hbs
    - Was added the meetup-list partial
    - Was added the logic for flip the card

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,90 @@ var router = express.Router();
 var labels = require('../dist/i18n/en/labels');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {"labels":labels});
+router.get('/', function(req, res, next) {
+    var content = {
+        labels: labels,
+        meetups: [
+            {
+                title: "Let's see the Sunset",
+                img: "images/events/beach.jpg",
+                abstract: "We are going to see the sunset drinking some beers. Please confirm before to go the event...",
+                description: "We are going to see the sunset drinking some beers, also we want to dance and talk about our experiences. Please confirm before to go the event",
+                date: "Wednesday, Aug 17",
+                hour: "7:00 PM",
+                address: {
+                    street: "Carrera 7 # 54-14",
+                    buildingName: "Atom House"
+                },
+                city: "Bogota",
+                price: "Free",
+                availability: "20 - 30"
+            },
+            {
+                title: "Let's see the Sunset",
+                img: "images/events/hiking.jpg",
+                abstract: "We are going to see the sunset drinking some beers. Please confirm before to go the event...",
+                description: "We are going to see the sunset drinking some beers, also we want to dance and talk about our experiences. Please confirm before to go the event",
+                date: "Wednesday, Aug 17",
+                hour: "7:00 PM",
+                address: {
+                    street: "Carrera 7 # 54-14",
+                    buildingName: "Atom House"
+                },
+                city: "Bogota",
+                price: "Free",
+                availability: "20 - 30"
+            },
+            {
+                title: "Let's see the Sunset",
+                img: "images/events/picnic.jpg",
+                abstract: "We are going to see the sunset drinking some beers. Please confirm before to go the event...",
+                description: "We are going to see the sunset drinking some beers, also we want to dance and talk about our experiences. Please confirm before to go the event",
+                date: "Wednesday, Aug 17",
+                hour: "7:00 PM",
+                address: {
+                    street: "Carrera 7 # 54-14",
+                    buildingName: "Atom House"
+                },
+                city: "Bogota",
+                price: "Free",
+                availability: "20 - 30"
+            },
+            {
+                title: "Let's see the Sunset",
+                img: "images/events/yoga.jpg",
+                abstract: "We are going to see the sunset drinking some beers. Please confirm before to go the event...",
+                description: "We are going to see the sunset drinking some beers, also we want to dance and talk about our experiences. Please confirm before to go the event",
+                date: "Wednesday, Aug 17",
+                hour: "7:00 PM",
+                address: {
+                    street: "Carrera 7 # 54-14",
+                    buildingName: "Atom House"
+                },
+                city: "Bogota",
+                price: "Free",
+                availability: "20 - 30"
+            },
+            {
+                title: "Let's see the Sunset",
+                img: "images/events/beach.jpg",
+                abstract: "We are going to see the sunset drinking some beers. Please confirm before to go the event...",
+                description: "We are going to see the sunset drinking some beers, also we want to dance and talk about our experiences. Please confirm before to go the event",
+                date: "Wednesday, Aug 17",
+                hour: "7:00 PM",
+                address: {
+                    street: "Carrera 7 # 54-14",
+                    buildingName: "Atom House"
+                },
+                city: "Bogota",
+                price: "Free",
+                availability: "20 - 30"
+            }
+        ]
+
+    };
+
+    res.render('index', content);
 });
 
 module.exports = router;
